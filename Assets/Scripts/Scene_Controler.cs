b2fc7244e92aad05c0db95cd0c5392deea9e0242
Abner <abner.matadamas@uteca.edu.mx>
2026-01-21 20:30:29 -0600

    Pasamos a programar el juego

---
+++ Assets/Scripts/Scene_Controler.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Scene_Controler : MonoBehaviour
+{
+    [SerializeField]
+
+    private Animator fade;
+    [SerializeField]
+
+    private string fadeAnimationName;
+
+    public void GotoSceneWithFade(string sceneName)
+    {
+        StartCoroutine(LoadSceneAfterFade(sceneName));
+    }
+
+    private IEnumerator LoadSceneAfterFade(string sceneName)
+    {
+        fade.Play(fadeAnimationName, 0 ,0f);
+        yield return new WaitForSeconds(fade.GetCurrentAnimatorStateInfo(0).length);
+        SceneManager.LoadScene(sceneName);
+
+    }
+
+}
